2eefb1929e80d430be61790880c4d66dc87543cf
pambrose <pambrose@mac.com>
2021-10-04 07:11:05 -0700

    Update SQL

--- src/main/resources/db/migration/V001__create_base_tables.sql
+++ src/main/resources/db/migration/V001__create_base_tables.sql
@@ -4,10 +4,9 @@ create schema operations;
 
 CREATE TABLE operations.users
 (
-    id         SERIAL UNIQUE PRIMARY KEY,
+    id         INT UNIQUE PRIMARY KEY,
     created    TIMESTAMPTZ DEFAULT NOW(),
     updated    TIMESTAMPTZ DEFAULT NOW(),
-    retool_id  TEXT NOT NULL UNIQUE,
     email      TEXT NOT NULL UNIQUE,
     first_name TEXT NOT NULL,
     last_name  TEXT NOT NULL,
@@ -27,3 +26,14 @@ CREATE TABLE operations.userhours
 
     CONSTRAINT user_date_unique unique (user_ref, date)
 );
+
+CREATE VIEW operations.user_goals AS
+SELECT id,
+       CASE
+           WHEN (graduation = 2022) THEN 220
+           WHEN (graduation = 2023) THEN 200
+           WHEN (graduation = 2024) THEN 180
+           WHEN (graduation = 2025) THEN 160
+           ELSE 100
+           END AS goal
+FROM operations.users;
